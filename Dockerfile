369db83e01ba7e1af4b36e824079e0495aab9345
albert_q <quentin.albertone@epitech.eu>
2020-05-14 15:03:34 +0200

    create docker image from dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
-FROM bitnami/kubectl:1.17.4
+FROM google/cloud-sdk:288.0.0
 
 COPY entrypoint.sh /entrypoint.sh
+
+RUN apt-get install -qqy jq
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,39 @@
-#!/bin/sh -l
+#!/bin/bash
 
-echo "kubeconfig :"
-echo "---------------------------------------------"
-echo "$1"
-echo "---------------------------------------------"
-echo "image :"
-echo "---------------------------------------------"
-echo "$2"
-echo "---------------------------------------------"
-echo "deployment :"
-echo "---------------------------------------------"
-echo "$3"
-echo "---------------------------------------------"
-echo "container :"
-echo "---------------------------------------------"
-echo "$4"
-echo "---------------------------------------------"
+# echo "json_key :"
+# echo "---------------------------------------------"
+# echo "$1"
+# echo "---------------------------------------------"
+# echo "cluster :"
+# echo "---------------------------------------------"
+# echo "$2"
+# echo "---------------------------------------------"
+# echo "image :"
+# echo "---------------------------------------------"
+# echo "$3"
+# echo "---------------------------------------------"
+# echo "deployment :"
+# echo "---------------------------------------------"
+# echo "$4"
+# echo "---------------------------------------------"
+# echo "container :"
+# echo "---------------------------------------------"
+# echo "$5"
+# echo "---------------------------------------------"
 
+# key= key in base64
+cluster=helium-alpha
 
-echo "::set-output name=current::{$2##*:}"
+# gcloud config configurations create MY_CONFIG
+# gcloud config configurations activate MY_CONFIG
+
+base64 --decode <<< $key > gcloud-service-key.json
+project=$(cat gcloud-service-key.json | jq '.project_id')
+account=$(cat gcloud-service-key.json | jq '.client_email')
+echo -e "=> project: $project"
+echo -e "=> account: $account"
+gcloud auth activate-service-account $account --key-file=./gcloud-service-key.json --project=$project
+# gcloud config set project $project
+# gcloud container clusters get-credentials $cluster --project $project
+# kubectl get nodes
+# echo "::set-output name=current::{$cluster##*:}"
