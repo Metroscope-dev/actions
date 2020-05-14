2ea5130bace4110cd444cf70147f7e05e3ed7629
albert_q <quentin.albertone@epitech.eu>
2020-05-14 17:00:48 +0200

    change entrypoint

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,160 +1,44 @@
 #!/bin/sh
 set -e
 
-function main() {
-  echo "" # see https://github.com/actions/toolkit/issues/168
-
-  sanitize "${INPUT_NAME}" "name"
-  sanitize "${INPUT_USERNAME}" "username"
-  sanitize "${INPUT_PASSWORD}" "password"
-
-  REGISTRY_NO_PROTOCOL=$(echo "${INPUT_REGISTRY}" | sed -e 's/^https:\/\///g')
-  if uses "${INPUT_REGISTRY}" && ! isPartOfTheName "${REGISTRY_NO_PROTOCOL}"; then
-    INPUT_NAME="${REGISTRY_NO_PROTOCOL}/${INPUT_NAME}"
-  fi
-
-  if uses "${INPUT_TAGS}"; then
-    TAGS=$(echo "${INPUT_TAGS}" | sed "s/,/ /g")
-  else 
-    translateDockerTag
-  fi
-
-  if uses "${INPUT_WORKDIR}"; then
-    changeWorkingDirectory
-  fi
-
-  echo ${INPUT_PASSWORD} | docker login -u ${INPUT_USERNAME} --password-stdin ${INPUT_REGISTRY}
-
-  FIRST_TAG=$(echo $TAGS | cut -d ' ' -f1)
-  DOCKERNAME="${INPUT_NAME}:${FIRST_TAG}"
-  BUILDPARAMS=""
-  CONTEXT="."
-
-  if uses "${INPUT_DOCKERFILE}"; then
-    useCustomDockerfile
-  fi
-  if uses "${INPUT_BUILDARGS}"; then
-    addBuildArgs
-  fi
-  if uses "${INPUT_CONTEXT}"; then
-    CONTEXT="${INPUT_CONTEXT}"
-  fi
-  if usesBoolean "${INPUT_CACHE}"; then
-    useBuildCache
-  fi
-  if usesBoolean "${INPUT_SNAPSHOT}"; then
-    useSnapshot
-  fi
-
-  push
-
-  echo "::set-output name=tag::${FIRST_TAG}"
-  DIGEST=$(docker inspect --format='{{index .RepoDigests 0}}' ${DOCKERNAME})
-  echo "::set-output name=digest::${DIGEST}"
-
-  docker logout
+function login { ## regirtry login password
+    echo "=> docker log to $1"
+    echo $3 | docker login -u $2 --password-stdin $1
 }
 
-function sanitize() {
-  if [ -z "${1}" ]; then
-    >&2 echo "Unable to find the ${2}. Did you set with.${2}?"
-    exit 1
-  fi
+function build { ## dockerfile image
+    echo "=> docker build $2"
+    echo "> dockerfile $1"
+    docker build -f $1 -t $2 .
 }
 
-function isPartOfTheName() {
-  [ $(echo "${INPUT_NAME}" | sed -e "s/${1}//g") != "${INPUT_NAME}" ]
+function push { ## image
+    echo "=> docker push $1"
+    docker push $1
 }
 
-function translateDockerTag() {
-  local BRANCH=$(echo ${GITHUB_REF} | sed -e "s/refs\/heads\///g" | sed -e "s/\//-/g")
-  if hasCustomTag; then
-    TAGS=$(echo ${INPUT_NAME} | cut -d':' -f2)
-    INPUT_NAME=$(echo ${INPUT_NAME} | cut -d':' -f1)
-  elif isOnMaster; then
-    TAGS="latest"
-  elif isGitTag && usesBoolean "${INPUT_TAG_SEMVER}" && isSemver "${GITHUB_REF}"; then
-    TAGS=$(echo ${GITHUB_REF} | sed -e "s/refs\/tags\///g" | sed -E "s/v?([0-9]+)\.([0-9+])\.([0-9]+)(-[a-zA-Z]+(\.[0-9]+)?)?/\1.\2.\3\4 \1.\2\4 \1\4/g")
-  elif isGitTag && usesBoolean "${INPUT_TAG_NAMES}"; then
-    TAGS=$(echo ${GITHUB_REF} | sed -e "s/refs\/tags\///g")
-  elif isGitTag; then
-    TAGS="latest"
-  elif isPullRequest; then
-    TAGS="${GITHUB_SHA}"
-  else
-    TAGS="${BRANCH}"
-  fi;
+function switch_image { ## image tag current_repo tag wanted_repo image
+    echo "> switch from repo $3 to $4"
+    image=$(echo "$1" | sed "s/harbor.metroscope.tech\/$3/harbor.metroscope.tech\/$4/g"):$2
 }
 
-function hasCustomTag() {
-  [ $(echo "${INPUT_NAME}" | sed -e "s/://g") != "${INPUT_NAME}" ]
-}
+password=$INPUT_REGISTRY_PASSWORD
+login=$INPUT_REGISTRY_LOGIN
+image=$INPUT_IMAGE
+tag=$INPUT_TAG
+dockerfile=$INPUT_DOCKERFILE
+registry=$(echo $image | cut -f1 -d"/")
 
-function isOnMaster() {
-  [ "${BRANCH}" = "master" ]
-}
+case "$tag" in
+    *"DEV"* | "latest")     echo "=> create and push dev version : $VERSION"
+                            switch_image "$image" "$tag" "prod" "dev"
+                            ;;
+    *)                      echo "=> create and push prod version : $VERSION"
+                            switch_image "$image" "$tag" "dev" "prod"
+                            ;;
+esac
+echo "> $image"
 
-function isGitTag() {
-  [ $(echo "${GITHUB_REF}" | sed -e "s/refs\/tags\///g") != "${GITHUB_REF}" ]
-}
-
-function isPullRequest() {
-  [ $(echo "${GITHUB_REF}" | sed -e "s/refs\/pull\///g") != "${GITHUB_REF}" ]
-}
-
-function changeWorkingDirectory() {
-  cd "${INPUT_WORKDIR}"
-}
-
-function useCustomDockerfile() {
-  BUILDPARAMS="${BUILDPARAMS} -f ${INPUT_DOCKERFILE}"
-}
-
-function addBuildArgs() {
-  for ARG in $(echo "${INPUT_BUILDARGS}" | tr ',' '\n'); do
-    BUILDPARAMS="${BUILDPARAMS} --build-arg ${ARG}"
-    echo "::add-mask::${ARG}"
-  done
-}
-
-function useBuildCache() {
-  if docker pull ${DOCKERNAME} 2>/dev/null; then
-    BUILDPARAMS="$BUILDPARAMS --cache-from ${DOCKERNAME}"
-  fi
-}
-
-function uses() {
-  [ ! -z "${1}" ]
-}
-
-function usesBoolean() {
-  [ ! -z "${1}" ] && [ "${1}" = "true" ]
-}
-
-function isSemver() {
-  echo "${1}" | grep -Eq '^refs/tags/v?([0-9]+)\.([0-9+])\.([0-9]+)(-[a-zA-Z]+(\.[0-9]+)?)?$'
-}
-
-function useSnapshot() {
-  local TIMESTAMP=`date +%Y%m%d%H%M%S`
-  local SHORT_SHA=$(echo "${GITHUB_SHA}" | cut -c1-6)
-  local SNAPSHOT_TAG="${TIMESTAMP}${SHORT_SHA}"
-  TAGS="${TAGS} ${SNAPSHOT_TAG}"
-  echo ::set-output name=snapshot-tag::"${SNAPSHOT_TAG}"
-}
-
-function push() {
-  local BUILD_TAGS=""
-  for TAG in ${TAGS}
-  do
-    BUILD_TAGS="${BUILD_TAGS}-t ${INPUT_NAME}:${TAG} "
-  done
-  docker build ${INPUT_BUILDOPTIONS} ${BUILDPARAMS} ${BUILD_TAGS} ${CONTEXT}
-
-  for TAG in ${TAGS}
-  do
-    docker push "${INPUT_NAME}:${TAG}"
-  done
-}
-
-main
+login $registry $login $password
+build $dockerfile $image
+push  $image
