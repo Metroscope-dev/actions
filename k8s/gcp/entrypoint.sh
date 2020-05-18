2ed28c01fb4471c9a59dc1d9fad266e626851b3f
albert_q <quentin.albertone@epitech.eu>
2020-05-18 18:51:34 +0200

    retry with empty function

--- k8s/gcp/entrypoint.sh
+++ k8s/gcp/entrypoint.sh
@@ -1,46 +1,60 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-update () { ## deployment container image
-    echo "=> Update deployment $1/$2 image: $3"
-    kubectl set image "deployment/$1 $2=$3" --record
-}
+# update () { ## deployment container image
+#     echo "=> Update deployment $1/$2 image: $3"
+#     kubectl set image "deployment/$1 $2=$3" --record
+# }
 
-switch_image () { ## image tag current_repo wanted_repo
-    echo "> switch from repo $3 to $4"
-    image=$(echo "$1" | sed "s/harbor.metroscope.tech\/$3/harbor.metroscope.tech\/$4/g"):$2
-}
+# switch_image () { ## image tag current_repo wanted_repo
+#     echo "> switch from repo $3 to $4"
+#     image=$(echo "$1" | sed "s/harbor.metroscope.tech\/$3/harbor.metroscope.tech\/$4/g"):$2
+# }
 
-get_credentials () {
-    if [ ! -d "$HOME/.config/gcloud" ]; then
-        if [ -z "${APPLICATION_CREDENTIALS-}" ]; then
-            echo "APPLICATION_CREDENTIALS not found. Exiting...."
-            exit 1
-        fi
-        echo "$INPUT_APPLICATION_CREDENTIALS" | base64 -d > /tmp/account.json
-        project_id=$(cat /tmp/account.json | jq '.project_id')
-        gcloud auth activate-service-account --key-file=/tmp/account.json --project "$project_id"
-    fi
-    echo ::add-path::/google-cloud-sdk/bin/gcloud
-    echo ::add-path::/google-cloud-sdk/bin/gsutil
+# case "$INPUT_TAG" in
+#     *"DEV"* | "latest")     echo "=> create and push dev version : $VERSION"
+#                             switch_image "$image" "$tag" "prod" "dev"
+#                             ;;
+#     *)                      echo "=> create and push prod version : $VERSION"
+#                             switch_image "$image" "$tag" "dev" "prod"
+#                             ;;
+# esac
 
-    # Update kubeConfig.
-    gcloud container clusters get-credentials "$CLUSTER_NAME" --project "$project_id" --zone "$ZONE"
-}
+if [ ! -d "$HOME/.config/gcloud" ]; then
+   if [ -z "${APPLICATION_CREDENTIALS-}" ]; then
+      echo "APPLICATION_CREDENTIALS not found. Exiting...."
+      exit 1
+   fi
 
-image=$INPUT_IMAGE
-registry=$(echo $INPUT_IMAGE | cut -f1 -d"/")
+   if [ -z "${PROJECT_ID-}" ]; then
+      echo "PROJECT_ID not found. Exiting...."
+      exit 1
+   fi
+
+   echo "$APPLICATION_CREDENTIALS" | base64 -d > /tmp/account.json
+
+   project_id=$(cat /tmp/account.json | jq -r '.project_id')
+   echo "project ID: $project_id"
+   echo "project ID: $project_id" | cat -e
+   gcloud auth activate-service-account --key-file=/tmp/account.json --project $project_id
+
+fi
+
+echo "project ID: $project_id"
+echo "project ID: $project_id" | cat -e
 
 
-case "$INPUT_TAG" in
-    *"DEV"* | "latest")     echo "=> create and push dev version : $VERSION"
-                            switch_image "$image" "$tag" "prod" "dev"
-                            ;;
-    *)                      echo "=> create and push prod version : $VERSION"
-                            switch_image "$image" "$tag" "dev" "prod"
-                            ;;
-esac
+echo ::add-path::/google-cloud-sdk/bin/gcloud
+echo ::add-path::/google-cloud-sdk/bin/gsutil
 
-get_credentials
-echo "> $image"
-kubectl get nodes
+# Update kubeConfig.
+gcloud container clusters get-credentials "$CLUSTER_NAME" --zone "$ZONE_NAME" --project "$PROJECT_ID"
+
+# verify kube-context
+kubectl config current-context
+kubectl get deployment
+# sh -c "kubectl $*"
+
+# sh -c "kubectl "
+# image=$INPUT_IMAGE
+# registry=$(echo $INPUT_IMAGE | cut -f1 -d"/")
