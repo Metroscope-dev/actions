bd58655391688391d9e8ba6669a268867b733f54
albert_q <quentin.albertone@epitech.eu>
2020-05-22 12:32:38 +0200

    change description build and create tagging action

---
+++ tagging/entrypoint.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+set -o errexit
+
+latestTag=''
+latestIncrement=0
+
+targetTag=''
+targetIncrement=latestIncrement
+
+setTargetTag() {
+    # Find current year and month
+    currentYear=$(date +'%y')
+    currentMonth=$(date +'%m')
+
+    # Find latest tag corresponding to current year and month
+    latestTag=$(git tag | grep "${currentYear}\.${currentMonth}\-DEV.*" | sort | tail -n 1)
+    latestIncrement=$(echo "${latestTag}" | sed "s/${currentYear}\.${currentMonth}\-DEV//" | sed 's/^0*//')
+    echo "Latest tag for this month: '${latestTag}'"
+
+    # If latestTag is not null, increment targetIncrement by 1 and format using 3 digits
+    if [ -z "${latestIncrement}" ]; then
+      targetIncrement=$(printf "%03d" 1)
+    else
+      targetIncrement=$((latestIncrement + 1))
+      targetIncrement=$(printf "%03d" ${targetIncrement})
+    fi
+
+    # Define target tag
+    targetTag="${currentYear}.${currentMonth}-DEV${targetIncrement}"
+    echo "Target tag: '${targetTag}'"
+}
+
+performTagging() {
+    echo "Creating and pushing tag: '${targetTag}'"
+    echo ::set-output name=tag::$targetTag
+}
+
+
+git fetch --all --tags
+if [ $# == 1 ] && [ ! -z $1 ];
+then
+    targetTag=$1
+else
+    setTargetTag
+fi
+performTagging
