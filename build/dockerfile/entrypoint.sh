8fd98dd74ad23c623d663bac393f9de1a4be83e4
albert_q <quentin.albertone@epitech.eu>
2020-08-28 11:11:45 +0200

    add tag env variable

--- build/dockerfile/entrypoint.sh
+++ build/dockerfile/entrypoint.sh
@@ -12,8 +12,9 @@ function build { ## dockerfile image tag
 }
 
 function push { ## image tag
-    echo "=> docker push $1:$2"
+    echo "=> docker push $1:[ $2, latest ]"
     docker push $1:$2
+    docker push $1:latest
 }
 
 function switch_image { ## image current_repo wanted_repo
@@ -21,14 +22,14 @@ function switch_image { ## image current_repo wanted_repo
     image=$(echo "$1" | sed "s/harbor.metroscope.tech\/$2/harbor.metroscope.tech\/$3/g")
 }
 
-password=$INPUT_REGISTRY_PASSWORD
-login=$INPUT_REGISTRY_LOGIN
-image=$INPUT_IMAGE
-github_ref=${GITHUB_REF}
-tag=${github_ref/refs\/tags\//}
-# tag=$INPUT_TAG
-dockerfile=$INPUT_DOCKERFILE
-registry=$(echo $image | cut -f1 -d"/")
+# password=$INPUT_REGISTRY_PASSWORD
+# login=$INPUT_REGISTRY_LOGIN
+# image=$INPUT_IMAGE
+# github_ref=${GITHUB_REF}
+# tag=${github_ref/refs\/tags\//}
+# # tag=$INPUT_TAG
+# dockerfile=$INPUT_DOCKERFILE
+registry=$(echo $INPUT_IMAGE | cut -f1 -d"/")
 
 ## TODO delete latest because it's a defautl
 case "$tag" in
@@ -41,7 +42,6 @@ case "$tag" in
 esac
 echo "> $image"
 
-login $registry $login $password
-build $dockerfile $image $tag
-push  $image $tag
-push  $image "latest"
+login $registry $INPUT_REGISTRY_LOGIN $INPUT_REGISTRY_PASSWORD
+build $INPUT_DOCKERFILE $INPUT_IMAGE $INPUT_TAG
+push  $INPUT_IMAGE $INPUT_TAG
