225e4de084ce7016d7f77d3ad80feb96fda1e581
albert_q <quentin.albertone@epitech.eu>
2021-02-08 14:09:17 +0100

    build/pypi : correct typo + delete debug

--- build/pypi/entrypoint.sh
+++ build/pypi/entrypoint.sh
@@ -6,7 +6,6 @@ PACKAGE_NAME=$(python3 setup.py --name)
 
 echo "=> Build and push $PACKAGE_NAME==$PACKAGE_VERSION"
 sed -i "s/version=.*/version='$PACKAGE_VERSION',/g" setup.py
-cat setup.py
 
 echo "=> Check if $PACKAGE_NAME==$PACKAGE_VERSION"
 version_already_on_remote_repo=$(pip3 install --index-url https://${INPUT_JFROG_USERNAME}:${INPUT_JFROG_PASSWORD}@metroscope.jfrog.io/metroscope/api/pypi/python-repo/simple ${PACKAGE_NAME}==notexisting 2>&1 | grep "Could not find a version that satisfies the requirement" | grep "[^0-9.]${PACKAGE_VERSION}[^0-9.]" | wc -l)
@@ -24,7 +23,6 @@ pip3 install \
      --extra-index-url https://pypi.python.org/simple .
 echo "=> Create all-requirements.txt"
 pip3 list --format=freeze > all-requirements.txt
-cat all-requirements.txt
 
 echo "=> Build $PACKAGE_NAME==$PACKAGE_VERSION"
 python3 setup.py sdist bdist_wheel
