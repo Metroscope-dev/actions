d10e992f4316b238c7b7331e4585a20d7e924aa8
Gaspard CHARLES <gaspard.charles@metroscope.tech>
2021-02-25 14:32:18 +0100

    Added release dependencies, remove setup.py version inplace change and changed pip freeze command

--- build/pypi/entrypoint.sh
+++ build/pypi/entrypoint.sh
@@ -5,7 +5,6 @@ PACKAGE_VERSION=${INPUT_PACKAGE_VERSION##*/}
 PACKAGE_NAME=$(python3 setup.py --name)
 
 echo "=> Build and push $PACKAGE_NAME==$PACKAGE_VERSION"
-sed -i "s/version=.*/version='$PACKAGE_VERSION',/g" setup.py
 
 echo "=> Check if $PACKAGE_NAME==$PACKAGE_VERSION"
 version_already_on_remote_repo=$(pip3 install --index-url https://${INPUT_JFROG_USERNAME}:${INPUT_JFROG_PASSWORD}@metroscope.jfrog.io/metroscope/api/pypi/python-repo/simple ${PACKAGE_NAME}==notexisting 2>&1 | grep "Could not find a version that satisfies the requirement" | grep "[^0-9.]${PACKAGE_VERSION}[^0-9.]" | wc -l)
@@ -17,15 +16,20 @@ if [ ${version_already_on_remote_repo} = 1 ]; then
     exit 1
 fi
 
+echo "=> Update pip and install build dependencies"
+pip3 install -U pip
+pip3 install setuptools>=53.0.0 wheel>=0.36.2
+
 echo "=> Install local package"
 pip3 install \
      --index-url https://${INPUT_JFROG_USERNAME}:${INPUT_JFROG_PASSWORD}@metroscope.jfrog.io/metroscope/api/pypi/python-repo/simple \
      --extra-index-url https://pypi.python.org/simple .
+
 echo "=> Create all-requirements.txt"
-pip3 list --format=freeze > all-requirements.txt
+pip3 freeze --all > all-requirements.txt
 
 echo "=> Build $PACKAGE_NAME==$PACKAGE_VERSION"
 python3 setup.py sdist bdist_wheel
 
 echo "=> Build $PACKAGE_NAME==$PACKAGE_VERSION"
-twine upload --repository-url https://metroscope.jfrog.io/metroscope/api/pypi/python-repo/ --username ${INPUT_JFROG_USERNAME} --password ${INPUT_JFROG_PASSWORD} dist/*.tar.gz
+twine upload --repository-url https://metroscope.jfrog.io/metroscope/api/pypi/python-repo/ --username ${INPUT_JFROG_USERNAME} --password ${INPUT_JFROG_PASSWORD} dist/*
