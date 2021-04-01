5b920d1c220affacf2e9a5bc57f8f4e324883fec
albert_q <quentin.albertone@epitech.eu>
2021-04-01 16:07:38 +0200

    pull, kustomize update, push on a github repo

---
+++ k8s/gitops/kustomize/entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+function clone_repository { # github_personal_acces_token repository_link
+    repo_access=$(echo "$2" | sed "s/:\/\//:\/\/$1@/g" -)
+    echo "=> git clone $repo_access"
+    git clone -b $3 --single-branch $repo_access
+}
+
+function update_kustomize { # kustomize_path image new_tag old_tag
+    cd $1
+    kustomize edit set image $2:$4=$2:$3
+    git diff kustomization.yaml
+    cd -
+}
+
+function push_repository { # image new_tag
+    git config --global user.email "action@github.com"
+    git config --global user.name "GitHub Action"
+    git commit -am "[skip ci] Update $1:$2"
+    git push
+}
+
+clone_repository $INPUT_PAT $INPUT_REPO_LINK $INPUT_BRANCH
+cd $(echo ${INPUT_REPO_LINK##*/} | sed 's/.git//g' -)
+for p in $(echo $INPUT_PATH | sed "s/,/ /g")
+do
+    echo "------"
+    update_kustomize $p $INPUT_IMAGE $INPUT_NEW_TAG $INPUT_OLD_TAG
+done
+
+# push_repository $INPUT_IMAGE $INPUT_NEW_TAG
